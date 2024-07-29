77e37f81c2a0372a3b081158c42414c363756913
Jayanto211 <jayantomondal2113@gmail.com>
2024-07-29 13:32:51 +0530

    Game1

---
+++ guessing game.py
@@ -0,0 +1,17 @@
+#Guessing guessing game
+
+import random
+target= random.randint(1,100)
+while True:
+    a=input("Guess The Number or Enter (X) To Quit : ")
+    if(a=="x"):
+        break
+    a=int(a)
+    if(a==target):
+        print("Congrts You Have Guessed Number")
+        break
+    elif(a<target):
+        print("Enter Bigger Number ")
+    else:
+        print("Enter Smaller Number ")
+print("#### Game Over ####")
